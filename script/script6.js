7667ef6a41251d53a59a7cce05a273222b47455e
Alex Sicart Ramos <alexsicart@MacBook-Pro-de-Alex.local>
2017-04-16 01:41:43 +0200

    jquery6

--- script/script6.js
+++ script/script6.js
@@ -2,18 +2,9 @@ $(document).ready(function()
 	{
 	$(".container .right .write .write-link.send").click(function () {
 	var data = $(".container .right .write input").val();
-  $('.container .right .chat .name').append(function(){
-    var joan = ['Hello', 'Goodbye', 'Bye', 'How are you?', 'What are you doing?', 'A dreamer', '!', 'Do it!', 'Ahhhh' ];
+  $('.container .right .chat .name').append('Alex says: ' + ' ' + data + ' .<br/><br/> ');
 
-    return  'Alex says: ' + ' ' + data + ' .<br/><br/> ';
-
-    $('.container .right .chat .name').append(function(){
-
-      return 'Joan says: ' + ' ' + joan[Math.floor(Math.random()*9)] + ' .<br/><br/> ';
-
-    });
-
-
-    });
 	});
+  $( ".container .right .chat .name" ).after('Joan says: ' + ' ' + joan[Math.floor(Math.random()*9)] + ' .<br/><br/> ');
+
 });
