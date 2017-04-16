8d0db3df53df48e1f1460fa35a8d679c01f8cd18
Alex Sicart Ramos <alex@sharge.io>
2017-04-16 02:19:59 +0200

    Changing Script6

--- script/script6.js
+++ script/script6.js
@@ -1,10 +1,14 @@
 $(document).ready(function()
 	{
+      var joan = ['Hello', 'Goodbye', 'Bye', 'How are you?', 'What are you doing?', 'A dreamer', '!', 'Do it!', 'Ahhhh' ];
+
 	$(".container .right .write .write-link.send").click(function () {
+    
 	var data = $(".container .right .write input").val();
+
   $('.container .right .chat .name').append('Alex says: ' + ' ' + data + ' .<br/><br/> ');
+  $( ".container .right .chat .name" ).after('Joan says: ' + ' ' + joan[Math.floor(Math.random()*9)] + ' .<br/><br/> ');
 
 	});
-  $( ".container .right .chat .name" ).after('Joan says: ' + ' ' + joan[Math.floor(Math.random()*9)] + ' .<br/><br/> ');
 
 });
